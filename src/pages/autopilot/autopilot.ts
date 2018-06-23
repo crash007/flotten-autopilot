2f5a70e7dd5473897c643bf89e89310f1263f86b
Petter Wallin <wallin.petter@gmail.com>
2018-06-23 21:43:34 +0200

    markers och linje

--- src/pages/autopilot/autopilot.ts
+++ src/pages/autopilot/autopilot.ts
@@ -1,10 +1,16 @@
-import { Component} from '@angular/core';
-import { NavController} from 'ionic-angular';
+import { Component } from '@angular/core';
+import { NavController, ToastController } from 'ionic-angular';
 
 import {
   GoogleMaps,
   GoogleMap,
-  GoogleMapOptions
+  GoogleMapOptions,
+  MyLocation,
+  GoogleMapsAnimation,
+  Marker,
+  GoogleMapsEvent,
+  Polyline,
+  LatLng
 } from '@ionic-native/google-maps';
 
 
@@ -15,28 +21,120 @@ import {
 export class Autopilot {
 
   map: GoogleMap;
- 
-  constructor(public navCtrl: NavController) {  
-    
+  points: Array<LatLng>;
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
+
   }
 
   ionViewDidLoad() {
-    this.loadMap();  
+    this.loadMap();
   }
-  
+
 
   loadMap() {
+
     console.log("load map");
     let options: GoogleMapOptions = {
       controls: {
         compass: true,
         myLocation: true,
         myLocationButton: true,
-        mapToolbar: true
-      }
+        mapToolbar: true,
+      },
+
+
     };
 
-    this.map = GoogleMaps.create('map_canvas', options );
+    this.map = GoogleMaps.create('map_canvas', options);
 
+    this.map.getMyLocation()
+      .then((location: MyLocation) => {
+        console.log(JSON.stringify(location, null, 2));
+        this.map.animateCamera({
+          target: location.latLng,
+          zoom: 15,
+          tilt: 30
+        })
+      }).then(() => {
+        this.map.on(GoogleMapsEvent.MAP_CLICK).subscribe((data: any) => {
+          this.map.clear();
+          console.log("map click");
+          let position: LatLng = <LatLng>data[0];
+          console.log(JSON.stringify(position, null, 2));
+          let marker: Marker = this.map.addMarkerSync({
+            title: 'Goal',
+            position: position,
+            animation: GoogleMapsAnimation.BOUNCE
+          });
+
+          marker.showInfoWindow();
+
+          // If clicked it, display the alert
+          marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+            this.showToast('clicked!');
+          });
+
+          this.map.getMyLocation()
+            .then((location: MyLocation) => {
+              let START = location.latLng;
+              let END = position;
+              
+               this.points = [
+                START,
+                END
+              ];
+
+              let polyline: Polyline = this.map.addPolylineSync({
+                points: this.points,
+                color: '#AA00FF',
+                width: 2,
+                geodesic: false,
+                clickable: true  // clickable = false in default
+              });
+            });
+
+
+        });
+      });
+
+    /*this.map.on(GoogleMapsEvent.MAP_CLICK).subscribe((data) => {
+      console.log("map click");
+      //this.placeMarkerAndPanTo(data.latLng, this.map);
+      /*
+      let marker: Marker = this.map.addMarkerSync({
+        title: 'Goal',
+        snippet: '',
+        position: data.latLng,
+        animation: GoogleMapsAnimation.BOUNCE
+      });
+
+      // show the infoWindow
+      marker.showInfoWindow();
+
+      // If clicked it, display the alert
+      marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+        this.showToast('clicked!');
+      });
+      */
+    //});
+
+  }
+
+
+
+  onButtonClick() {
+    
+  }
+
+
+  showToast(message: string) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 2000,
+      position: 'middle'
+    });
+
+    toast.present(toast);
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,14 +10,18 @@ import {Relay} from './relay.model';
 
 export class HomePage {
 
-  constructor(public navCtrl: NavController, private bluetoothSerial: BluetoothSerial, private babordRelay: Relay, private styrbordRelay: Relay) {
-    bluetoothSerial.enable();
-    
-    babordRelay.start = [0xA0, 0x01,0x01,0xA2];
-    babordRelay.stop = [0xA0, 0x01,0x00,0xA1];
+  babordRelay : Relay;
+  styrbordRelay: Relay;
 
-    styrbordRelay.start = [0xA0, 0x02,0x01,0xA3];
-    styrbordRelay.stop = [0xA0, 0x02,0x00,0xA2];
+  constructor(public navCtrl: NavController, private bluetoothSerial: BluetoothSerial) {
+    bluetoothSerial.enable();
+    this.babordRelay = new Relay;
+    this.styrbordRelay = new Relay;
+    this.babordRelay.start = [0xA0, 0x01,0x01,0xA2];
+    this.babordRelay.stop = [0xA0, 0x01,0x00,0xA1];
+
+    this.styrbordRelay.start = [0xA0, 0x02,0x01,0xA3];
+    this.styrbordRelay.stop = [0xA0, 0x02,0x00,0xA2];
 
   }
 
